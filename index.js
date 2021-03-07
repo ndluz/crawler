dc051c86afdce7aac9d8ad54788b57cdb0df8bf1
Anderson Luz <anderson54644@gmail.com>
2021-03-07 18:11:56 -0300

    ...

--- index.js
+++ index.js
@@ -5,17 +5,40 @@ const puppeteer =  require('puppeteer');
   const page = await browser.newPage();
   await page.goto('http://receitasdeminuto.com/categoria/receitas/lanches/');
   
-  await page.evaluate(() => {
+  const paginas = await page.evaluate(() => {
     const nodeList = document.querySelectorAll('h2 a');
     const postsArray = [...nodeList]
     const list  = postsArray.map( a => ({
       href: a.href
     }))
     
-    console.log(list)
-   
-
+    return list
   })
+  console.log(paginas)
+  
+  const receitas = []
+
+  for(let i = 0; i<paginas.length; i++){
+
+    await page.goto(paginas[i].href);
+
+    const pedaco = await page.evaluate(() => {
+
+      const nodeList = document.getElementsByClassName('postContent');
+
+      const conteudoReceita = [...nodeList];
+      
+      const receita = {
+        img: conteudoReceita[0].children[10].currentSrc       
+      }
+      return receita
+    })
+
+    receitas.push(pedaco)
+    console.log(receitas)
+  }
+
+  console.log(receitas);
 
   // await browser.close();
 })();
